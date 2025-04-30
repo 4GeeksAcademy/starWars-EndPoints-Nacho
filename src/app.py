cb06e693cfc6c491a6fe97ab1ebe5c440ceb6843
Nach91Mt <118282781+Nach91Mt@users.noreply.github.com>
2025-04-30 10:27:54 +0000

    first end points

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet
+from sqlalchemy.exc import IntegrityError
 #from models import Person
 
 app = Flask(__name__)
@@ -36,15 +37,68 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_all_user():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users= User.query.all()
+    return jsonify([user.serialize() for user in users]), 200
 
-    return jsonify(response_body), 200
+@app.route("/planets", methods=['get'])
+def get_all_planets():
+    planets= Planet.query.all()
+    return jsonify([planet.serialize() for planet in planets]), 200
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    data=request.get_json()
+    first_name=data.get("first_name")
+    last_name=data.get("last_name")
+    email=data.get("email")
+    password=data.get("password")
+    if not( first_name and last_name and email and password):
+        return jsonify({"error":"Faltan campos obligados "}), 400
+    user = User(first_name=first_name, last_name= last_name, email=email, password=password)
+    db.session.add(user)
+    try:
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return jsonify({"error": "El email ya esta registrado"}), 409
+
+    return jsonify({
+        "first_name":user.first_name,
+        "last_name":user.last_name,
+        "email":user.email,
+        "id":user.id
+    }) ,200
+
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user= User.query.get_or_404(user_id)
+    return jsonify(user.serialize())
+
+
+@app.route('/planet',methods=['POST'])
+def create_planet():
+    data= request.get_json()
+    name=data.get("name")
+    picture_url=data.get("picture_url")
+    users_ids=data.get("users",[])
+
+    if not name:
+        return jsonify({"error":"El nombre es obligatorio"})
+    planet= Planet(name=name,picture_url=picture_url)
+    db.session.add(planet)
+    db.session.commit()
+
+    for user_id in users_ids:
+        user= User.query.get(user_id)
+        if user: 
+            planet.users.append(user)
+        
+    db.session.commit()
+    return jsonify({"message": "Planet created"}), 201
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,19 +1,72 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Table, String, Column, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+user_planet = Table(
+    'user_planet',
+    db.Model.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('planet_id', Integer, ForeignKey('planet.id'), primary_key=True)
+)
 
+user_people = Table(
+    'user_people',
+    db.Model.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('people_id', Integer, ForeignKey('people.id'), primary_key=True)
+)
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    first_name: Mapped[str] = mapped_column(String(120), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(120), nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+
+    planets = relationship('Planet', secondary=user_planet, back_populates='users')
+    people = relationship('People', secondary=user_people, back_populates='users')
 
     def serialize(self):
         return {
             "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "planets": [planet.id for planet in self.planets],
+            "people": [person.id for person in self.people]
+        }
+
+class Planet(db.Model):
+    __tablename__ = "planet"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    picture_url: Mapped[str] = mapped_column(String(255), nullable=True)
+
+    users = relationship('User', secondary=user_planet, back_populates="planets")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "users": [user.id for user in self.users]
+        }
+
+class People(db.Model):
+    __tablename__ = "people"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    users = relationship('User', secondary=user_people, back_populates="people")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "users": [user.id for user in self.users]
         }
